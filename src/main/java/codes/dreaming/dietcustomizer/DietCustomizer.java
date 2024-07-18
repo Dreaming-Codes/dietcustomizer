3dc1cfa751ac825b3fe6891bcfa1ff77736579c0
DreamingCodes <me@dreaming.codes>
2024-07-18 12:56:10 +0200

    Base implementation

--- src/main/java/codes/dreaming/dietcustomizer/DietCustomizer.java
+++ src/main/java/codes/dreaming/dietcustomizer/DietCustomizer.java
@@ -1,10 +1,51 @@
 package codes.dreaming.dietcustomizer;
 
+import com.typesafe.config.ConfigFactory;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import com.typesafe.config.Config;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class DietCustomizer implements ModInitializer {
+    private static final File CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("dietcustomizer.conf").toFile();
+
+    public static Config CONFIG;
+
+    {
+        loadConfig();
+    }
 
     @Override
     public void onInitialize() {
+        ServerLifecycleEvents.END_DATA_PACK_RELOAD.register((server, resourceManager, success) -> loadConfig());
+    }
+
+    private void loadConfig() {
+        writeDefaultConfig();
+        ConfigFactory.invalidateCaches();
+        CONFIG = ConfigFactory.parseFile(CONFIG_FILE);
+    }
+
+    private void writeDefaultConfig() {
+        if (CONFIG_FILE.exists()) {
+            return;
+        }
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("default-config.conf")) {
+            if (inputStream != null) {
+                Files.copy(inputStream, CONFIG_FILE.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } else {
+                throw new IOException("Default configuration file not found in resources.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/codes/dreaming/dietcustomizer/mixin/DietApiImplMixin.java
+++ src/main/java/codes/dreaming/dietcustomizer/mixin/DietApiImplMixin.java
@@ -1,4 +1,54 @@
 package codes.dreaming.dietcustomizer.mixin;
 
-public class DietApiImplMixin {
+import com.illusivesoulworks.diet.api.type.IDietGroup;
+import com.illusivesoulworks.diet.api.type.IDietResult;
+import com.illusivesoulworks.diet.common.DietApiImpl;
+import com.illusivesoulworks.diet.common.data.group.DietGroups;
+import com.illusivesoulworks.diet.common.util.DietResult;
+import com.typesafe.config.Config;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Mixin;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import static codes.dreaming.dietcustomizer.DietCustomizer.CONFIG;
+
+import java.util.*;
+
+
+@Mixin(value = DietApiImpl.class, remap = false)
+public abstract class DietApiImplMixin {
+    @Inject(method = "get(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)Lcom/illusivesoulworks/diet/api/type/IDietResult;", at = @At("HEAD"), cancellable = true)
+    private void onGet(PlayerEntity player, ItemStack input, CallbackInfoReturnable<IDietResult> cir) {
+        Config foodValues = CONFIG.getConfig("foodValues");
+        String escapedId = "\"%s\"".formatted(Registry.ITEM.getId(input.getItem()).toString());
+        if (!foodValues.hasPath(escapedId)) {
+            cir.setReturnValue(new DietResult(new HashMap<>()));
+            return;
+        }
+        Config food = foodValues.getConfig(escapedId);
+        Set<IDietGroup> groups =  DietGroups.getGroups(player.getWorld());
+
+        HashMap<IDietGroup, Float> diet = new HashMap<>();
+
+        for (IDietGroup group : groups) {
+            if (!food.hasPath(group.getName())) {
+                continue;
+            }
+            diet.put(group, food.getNumber(group.getName()).floatValue());
+        }
+
+        cir.setReturnValue(new DietResult(diet));
+    }
+
+    // Since we are not using the default diet system, we can cancel the calculation of the diet just to be sure that it won't interfere with our custom diet system
+    @Inject(method = "calculate", at = @At("HEAD"), cancellable = true)
+    private static void onCalculate(float healing, float saturation, Set<IDietGroup> groups, CallbackInfoReturnable<Map<IDietGroup, Float>> cir) {
+
+        cir.setReturnValue(new HashMap<>());
+    }
+
 }
