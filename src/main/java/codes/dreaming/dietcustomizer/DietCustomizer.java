f4270059f3b4eba8c8e120c5abde0f5925243454
DreamingCodes <me@dreaming.codes>
2024-07-18 12:55:43 +0200

    Initial commit

---
+++ src/main/java/codes/dreaming/dietcustomizer/DietCustomizer.java
@@ -0,0 +1,10 @@
+package codes.dreaming.dietcustomizer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DietCustomizer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
