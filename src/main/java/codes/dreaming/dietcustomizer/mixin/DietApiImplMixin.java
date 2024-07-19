5e5055337533cfed19c4558cd3582a82732108cc
DreamingCodes <me@dreaming.codes>
2024-07-19 16:29:07 +0200

    fix tags

--- src/main/java/codes/dreaming/dietcustomizer/mixin/DietApiImplMixin.java
+++ src/main/java/codes/dreaming/dietcustomizer/mixin/DietApiImplMixin.java
@@ -6,6 +6,8 @@ import com.illusivesoulworks.diet.common.DietApiImpl;
 import com.illusivesoulworks.diet.common.data.group.DietGroups;
 import com.illusivesoulworks.diet.common.util.DietResult;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigValue;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tag.TagKey;
@@ -28,18 +30,20 @@ public abstract class DietApiImplMixin {
     private void onGet(PlayerEntity player, ItemStack input, CallbackInfoReturnable<IDietResult> cir) {
         HashMap<IDietGroup, Float> diet = new HashMap<>();
 
-        Config food = getFoodValues(input);
+        ConfigValue food = getFoodValues(input);
         if (food == null) {
             cir.setReturnValue(new DietResult(diet));
             return;
         }
+        HashMap<String, Number> foodValues = (HashMap<String, Number>) food.unwrapped();
 
         Set<IDietGroup> groups =  DietGroups.getGroups(player.getWorld());
         for (IDietGroup group : groups) {
-            if (!food.hasPath(group.getName())) {
+            Number nutritionValue = foodValues.get(group.getName());
+            if (nutritionValue == null) {
                 continue;
             }
-            diet.put(group, food.getNumber(group.getName()).floatValue());
+            diet.put(group, nutritionValue.floatValue());
         }
 
         cir.setReturnValue(new DietResult(diet));
@@ -52,21 +56,20 @@ public abstract class DietApiImplMixin {
     }
 
     @Unique
-    private static Config getFoodValues(ItemStack itemStack) {
+    private static ConfigValue getFoodValues(ItemStack itemStack) {
         Config foodValues = CONFIG.getConfig("foodValues");
-        Config foodTagsValues = CONFIG.getConfig("foodTagsValues");
+        ConfigObject foodTagsValues = CONFIG.getObject("foodTagsValues");
 
         String escapedId = "\"%s\"".formatted(Registry.ITEM.getId(itemStack.getItem()).toString());
         if (foodValues.hasPath(escapedId)) {
-            return foodValues.getConfig(escapedId);
+            return foodValues.getValue(escapedId);
         }
 
         var stream = foodTagsValues.entrySet().stream().filter(ConfigValueEntry ->
             itemStack.isIn(TagKey.of(Registry.ITEM.getKey(), Identifier.tryParse(ConfigValueEntry.getKey())))
         ).findFirst();
 
-        return stream.map(stringConfigValueEntry -> CONFIG.getConfig("foodTagsValues").getConfig(stringConfigValueEntry.getKey())).orElse(null);
-
+        return stream.map(Map.Entry::getValue).orElse(null);
     }
 
 }
